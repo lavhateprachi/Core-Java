ed01e79458da29b64eb459267b9537adf521c35b
lavhateprachi <109958062+lavhateprachi@users.noreply.github.com>
2023-10-06 17:45:20 +0530

    Add files via upload

---
+++ Day2/src/Pointer2D.java
@@ -0,0 +1,43 @@
+
+import java.lang.Math;
+class Pointer2D
+{
+	private int x;
+	private int y;
+	
+	
+	Pointer2D()
+	{
+		x=0;
+		y=0;
+	}
+	Pointer2D(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	
+	public String show()
+	{
+		return "x and y Co-ordinates are : "+this.x+" "+this.y;
+	}
+	boolean isEqual(Pointer2D p)
+	{
+		if(this.x==p.x && this.y==p.y)
+			return true;
+		else
+			return false;
+	}
+	
+	double calculateDistance(Pointer2D p1)
+	{
+		
+		double distance;
+		
+		distance=((p1.x-this.x)*(p1.x-this.x))-((p1.y-this.y)*(p1.y-this.y));
+		return Math.sqrt(distance);
+		
+		
+	}
+
+}
